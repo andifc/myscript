719204d1e18f989310846facf36001408e4b97d4
andiyt <63683412+andiyt@users.noreply.github.com>
2023-05-19 22:47:02 -0400

    Update and rename redirectwg.js to redirectwgu.js

--- redirectwgu.js
+++ redirectwgu.js
@@ -1,6 +1,6 @@
 /*Redirector------------------------------------------------------------------------------------>*/
 
-	var allowedHostnames = ["sssssssswqqz.blogspot.com", "www.youtube.com", "www.facebook.com", "twitter.com", "www.instagram.com", "pinterest.com", "api.whatsapp.com", "www.linkedin.com", "www.blogger.com", "blogger.googleusercontent.com", "1.bp.blogspot.com", "2.bp.blogspot.com", "3.bp.blogspot.com", "4.bp.blogspot.com", "t.me", "paypal.me", "timeline.line.me", "www.google.com", "policies.google.com", "discord.com", "discord.gg"];
+	var allowedHostnames = [blogUrl.split("/")[2], "www.youtube.com", "www.facebook.com", "twitter.com", "www.instagram.com", "pinterest.com", "api.whatsapp.com", "www.linkedin.com", "www.blogger.com", "blogger.googleusercontent.com", "1.bp.blogspot.com", "2.bp.blogspot.com", "3.bp.blogspot.com", "4.bp.blogspot.com", "t.me", "paypal.me", "timeline.line.me", "www.google.com", "policies.google.com", "discord.com", "discord.gg"];
 
 var allahref = document.getElementsByTagName("a");
 
