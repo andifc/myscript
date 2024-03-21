0dadd3beda3bce55313beb10f133e12db71b053a
Andi <63683412+andifc@users.noreply.github.com>
2024-03-21 17:09:14 -0400

    Add files via upload

---
+++ mysql.js
@@ -0,0 +1,48 @@
+class MysqlDB {
+    #BaseKey;
+    #BaseUri;
+    constructor(DbUrl, DbKey) {
+      this.#BaseUri = DbUrl;
+      this.#BaseKey = DbKey;
+    }
+  
+    async query(sqlQuery, params) {
+      const queryBase64 = btoa(sqlQuery);
+      const url = `${this.#BaseUri}`;
+  
+      if (!this._validateInput(params)) {
+        throw new Error("Datos invalidos.");
+      }
+  
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Key': this.#BaseKey
+          },
+          body: JSON.stringify({
+            query: queryBase64,
+            params: params,
+          }),
+        });
+  
+        if (response.ok) {
+          const data = await response.json();
+          return { status: true, data: data };
+        } else {
+          throw new Error(`Error al ejecutar la consulta: ${response.statusText}`);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        return { status: false, error: error.message };
+      }
+    }
+  
+    _validateInput(inputData) {
+      return Array.isArray(inputData) && inputData.length > 0;
+    }
+  }
+  
+
+  
